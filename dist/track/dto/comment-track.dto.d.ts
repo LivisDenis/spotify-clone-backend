62ad8140873cdfb8f8ff69a6c9e0ea098c967cfe
LivisDenis <deniskachumachenko1@gmail.com>
2022-10-12 20:13:57 +0300

    first commit

---
+++ dist/track/dto/comment-track.dto.d.ts
@@ -0,0 +1,5 @@
+export declare class CommentTrackDto {
+    readonly username: string;
+    readonly text: string;
+    readonly trackId: number;
+}
---
+++ src/track/schemas/track.schema.ts
@@ -0,0 +1,33 @@
+import {InjectModel, Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {Document, Model} from 'mongoose';
+import * as mongoose from "mongoose";
+import {Comment, CommentDocument} from "./comment.schema";
+
+export type TrackDocument = Track & Document;
+
+@Schema()
+export class Track {
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    artist: string;
+
+    @Prop()
+    text: string;
+
+    @Prop()
+    listens: number;
+
+    @Prop()
+    picture: string;
+
+    @Prop()
+    audio: string;
+
+    @Prop({type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]})
+    comments: Comment[];
+}
+
+export const TrackSchema = SchemaFactory.createForClass(Track);
